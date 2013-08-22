a9cabd51f5ec652db9dccd31c55a53350a103a40
Andres Riancho <andres.riancho@gmail.com>
2013-08-22 12:50:00 -0300

    Dump credentials is DONE!

--- core/dump_credentials/command.py
+++ core/dump_credentials/command.py
@@ -1,5 +1,7 @@
 import logging
 
+from boto.core.credentials import AllCredentialFunctions
+
 
 def cmd_arguments(subparsers):
     #
@@ -17,3 +19,18 @@ def cmd_handler(args):
     :param args: The command line arguments as parsed by argparse
     '''
     logging.debug('Starting dump-credentials')
+    get_credentials()
+    
+def get_credentials(persona='default'):
+    for cred_fn in AllCredentialFunctions:
+        credentials = cred_fn(persona=persona,
+                              access_key_name='access_key',
+                              secret_key_name='secret_key')
+        if credentials:
+            logging.info('Found credentials using "%s"' % cred_fn.__name__)
+            logging.info('  Access key: %s' % credentials.access_key)
+            logging.info('  Secret key: %s' % credentials.secret_key)
+            if credentials.token:
+                logging.info('  Token: %s' % credentials.token)
+            logging.info('')
+    return credentials
